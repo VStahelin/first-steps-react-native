0791bd7c4526c033f83acb6f550a2e6926b1d87a
Vitor Jose Stahelin <vitorjosestahelin@gmail.com>
2021-11-10 15:41:59 -0300

    Optimizing MinMax

--- src/components/MinMax.js
+++ src/components/MinMax.js
@@ -2,19 +2,12 @@ import React from 'react';
 import {Text} from 'react-native';
 import Style from './style';
 
-export default props => {
-  const max = 0;
-  const min = 0;
-  if (props.n1 > props.n2) {
-    this.max = props.n1;
-    this.min = props.n2;
-  } else {
-    this.max = props.n2;
-    this.min = props.n1;
-  }
+export default ({n1, n2}) => {
+  const max = n1 > n2 ? n1 : n2;
+  const min = n1 < n2 ? n1 : n2;
   return (
     <Text style={Style.H2}>
-      O valor {this.max} eh maior que o valo {this.min}
+      O valor {max} eh maior que o valor {min}
     </Text>
   );
 };
